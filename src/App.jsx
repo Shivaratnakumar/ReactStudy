6dcd4a76d77eec00c264e814e28c85b4f839d2c0
Shivaratnakumar <shivaratnakumarpatil@gmail.com>
2025-09-13 23:16:51 +0530

    Dynamic component rendering and cod erefactor

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,16 @@ import CustomCard from "./components/Practice/CustomCard";
 import Header from "./components/Header/Header";
 import ConceptItem from  "./components/CoreConcept/CoreConcept";
 import TabButton from "./components/TabButton";
+import {useState} from "react";
+import {EXAMPLES} from "./data";
 
 
 function App() {
+  const [selectedTab, setSelectedTab] = useState("");
+  const handleTabSelect = (tab) => {
+    console.log(`${tab} tab selected`);
+    setSelectedTab(tab);
+  };
   return (
     <div>
       <Header />      
@@ -15,24 +22,46 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <ConceptItem {...CORE_CONCEPTS[0]} />
-            <ConceptItem {...CORE_CONCEPTS[1]} />
-            <ConceptItem {...CORE_CONCEPTS[2]} />
-            <ConceptItem {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((concept) => (
+              <ConceptItem key={concept.title} {...concept} />
+            ))}
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={(() => handleTabSelect("components"))} active={selectedTab === "components"}>Components</TabButton>
+            <TabButton onSelect={(() => handleTabSelect("jsx"))} active={selectedTab === "jsx"}>JSX</TabButton>
+            <TabButton onSelect={(() => handleTabSelect("props"))} active={selectedTab === "props"}>Props</TabButton>
+            <TabButton onSelect={(() => handleTabSelect("state"))} active={selectedTab === "state"}>State</TabButton>
           </menu>
+          {!selectedTab && <CustomCard name="Custom Card">
+                    <p>Please select a tab</p>
+                  </CustomCard>}
+          {selectedTab === "components" && <CustomCard name="Components">
+                    <p>This is a components card</p>
+                  </CustomCard>}
+          {selectedTab === "jsx" && <CustomCard name="JSX">
+                    <p>This is a JSX card</p>
+                  </CustomCard>}
+          {selectedTab === "props" && <CustomCard name="Props">
+                    <p>This is a props card</p>
+                  </CustomCard>}
+          {selectedTab === "state" && <CustomCard name="State">
+                    <p>This is a state card</p>
+                  </CustomCard>}
+
+          {selectedTab && (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTab].title}</h3>
+              <p>{EXAMPLES[selectedTab].description}</p>
+              <code>{EXAMPLES[selectedTab].code}</code>
+            </div>
+          )}
         </section>
-        <CustomCard name="Custom Card">
+        {/* <CustomCard name="Custom Card">
           <p>This is a custom card</p>
-        </CustomCard>
+        </CustomCard> */}
         {/* <h2>Time to get started!</h2> */}
         {/* <p>Welcome on board of this course! You got this 💪</p>
         <User />
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,7 +1,18 @@
-export default function TabButton({children, onSelect}) {
+export default function TabButton({children, onSelect, active}) {
+
+  const handleClick = () => {
+    console.log("TabButton clicked");
+    onSelect();
+  };
+
   return (
     <li>
-      <button onClick={onSelect}>{children}</button>
+      <button 
+        onClick={handleClick}
+        className={active ? 'active' : ''}
+      >
+        {children}
+      </button>
     </li>
   );
 }
